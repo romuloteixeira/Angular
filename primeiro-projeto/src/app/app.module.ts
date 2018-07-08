475018ae6a479475059fcd6498b0b847969438f1
romulo.teixeira <romulo.teixeira@squadra.com.br>
2018-07-08 18:06:12 -0300

    My first component / Meu primeiro componente.

--- primeiro-projeto/src/app/app.module.ts
+++ primeiro-projeto/src/app/app.module.ts
@@ -2,10 +2,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { MeuPrimeiroComponent } from './meu-primeiro/meu-primeiro.component';
+//import { MeuPrimeiroComponent } from './meu-primeiro/meu-primeiro.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MeuPrimeiroComponent
   ],
   imports: [
     BrowserModule
---
+++ primeiro-projeto/src/app/meu-primeiro/meu-primeiro.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'meu-primeiro-component',
+    template: `
+    <p>Meu Primeiro component com Aunguar 6!</p>
+    `
+})
+
+export class MeuPrimeiroComponent{}
