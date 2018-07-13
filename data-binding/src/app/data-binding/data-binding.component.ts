7b7cdd823cce1f635911a87713bb162e4518530d
romulo.teixeira <romulo.teixeira@squadra.com.br>
2018-07-12 23:27:06 -0300

    Working with Data Binding / Trabalhando com data binding.

---
+++ data-binding/src/app/data-binding/data-binding.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-data-binding',
+  templateUrl: './data-binding.component.html',
+  styleUrls: ['./data-binding.component.css']
+})
+export class DataBindingComponent implements OnInit {
+
+  url: string = "https://www.linkedin.com/in/romulo-teixeira";
+  urlImagem: string = "http://lorempixel.com/400/200/nature/";
+  cursoAngular: boolean = true;
+
+
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  getValor(){
+    return 1;
+  }
+
+  getConectarPerfil(){
+    return true;
+  }
+
+}
