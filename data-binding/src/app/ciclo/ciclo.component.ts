597fef542b519b2510429eeefe0ed66e086749e1
romulo.teixeira <romulo.teixeira@squadra.com.br>
2018-07-28 00:06:43 -0300

    Angular life ciclo / Ciclo de vida do Angular.

---
+++ data-binding/src/app/ciclo/ciclo.component.ts
@@ -0,0 +1,62 @@
+import { 
+  Component, 
+  OnInit, 
+  OnChanges, 
+  DoCheck, 
+  AfterContentInit, 
+  AfterContentChecked, 
+  AfterViewInit, 
+  AfterViewChecked,
+  OnDestroy
+} from '@angular/core';
+
+@Component({
+  selector: 'app-ciclo',
+  templateUrl: './ciclo.component.html',
+  styleUrls: ['./ciclo.component.css']
+})
+export class CicloComponent implements OnInit, OnChanges, DoCheck, AfterContentInit, AfterContentChecked, 
+                                        AfterViewInit, AfterViewChecked, OnDestroy {
+
+  valorInicial: number = 10;
+  
+  constructor() { 
+    this.log('Construtor');
+  }
+
+  ngOnChanges(){
+    this.log('ngOnChanges');
+  }
+
+  ngOnInit() {
+    this.log('ngOnInit');
+  }
+
+  ngDoCheck(){
+    this.log('ngDoCheck');
+  }
+
+  ngAfterContentChecked(){
+    this.log('ngAfterContentChecked');
+  }
+
+  ngAfterContentInit(){
+    this.log('ngAfterContentInit');
+  }
+
+  ngAfterViewInit(){
+    this.log('ngAfterViewInit');
+  }
+
+  ngAfterViewChecked(){
+    this.log('ngAfterViewChecked');
+  }
+
+  ngOnDestroy(){
+    this.log('ngDestroy');
+  }
+
+  private log(mensagem){
+    console.log(mensagem);
+  }
+}
