d2648e45847961a163ad9d8f52d75d39d66610ba
romulo.teixeira <romulo.teixeira@squadra.com.br>
2018-07-28 16:49:13 -0300

    Using / Usando View Child

--- data-binding/src/app/output-property/output-property.component.ts
+++ data-binding/src/app/output-property/output-property.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input, EventEmitter, Output } from '@angular/core';
+import { Component, OnInit, Input, EventEmitter, Output, ViewChild, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'contador',
@@ -10,6 +10,7 @@ export class OutputPropertyComponent implements OnInit {
 
   @Input() valor: number = 0;
   @Output() mudouValor = new EventEmitter();
+  @ViewChild("campoInput") campoValorInput: ElementRef;
 
   constructor() { }
 
@@ -17,12 +18,15 @@ export class OutputPropertyComponent implements OnInit {
   }
 
   incrementa(){
-    this.valor++;
+    console.log(this.campoValorInput.nativeElement.value);
+    //this.valor++;
+    this.campoValorInput.nativeElement.value++;
     this.mudouValor.emit({ novoValor: this.valor });
   }
 
   decrementa(){
-    this.valor--;
+    //this.valor--;
+    this.campoValorInput.nativeElement.value--;
     this.mudouValor.emit({ novoValor: this.valor });
   }
 }
