27e65cf138f794e8a437119a7b18d8c4309eec73
romulo.teixeira <romulo.teixeira@squadra.com.br>
2018-08-07 23:06:31 -0300

    MaterializeCSS, Lodash etc.

--- angular-cli-libs-externas/src/app/app.module.ts
+++ angular-cli-libs-externas/src/app/app.module.ts
@@ -1,10 +1,13 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { BrowserModule } from '@angular/platform-browser';
 import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
 import { TooltipModule } from 'ngx-bootstrap/tooltip';
 import { ModalModule } from 'ngx-bootstrap/modal';
 
-import { AppComponent } from './app.component';
+import 'materialize-css';
+import { MaterializeModule } from 'angular2-materialize';
 
 @NgModule({
   declarations: [
@@ -14,7 +17,8 @@ import { AppComponent } from './app.component';
     BrowserModule,
     BsDropdownModule.forRoot(),
     TooltipModule.forRoot(),
-    ModalModule.forRoot()
+    ModalModule.forRoot(),
+    MaterializeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
